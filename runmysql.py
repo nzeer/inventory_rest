e9a2aeeff3eafb21b621ea145b20a5956a0bd553
nZeeR <nzeer@me.com>
2024-02-10 17:12:43 -0500

    cleaned up formatting

--- runmysql.py
+++ runmysql.py
@@ -26,35 +26,11 @@ def main():
     """
 
     try:
-        #if mysql_connection.execute_mysql_query(create_table_sql):
-        #    print("Table created successfully")
-        pass
+        for t in mysql_connection.get_mysql_tables():
+            for c in mysql_connection.get_mysql_columns(t):
+                print("(\ntable: %s)[field: %s\n]" % (t, c))
     except Exception as e:
         print(e)
-
-    #mysql_connection.show_mysql_tables()
-
-    for t in mysql_connection.get_mysql_tables():
-        for c in mysql_connection.get_mysql_columns(t):
-            print("(\ntable: %s)[field: %s\n]" % (t, c))
-        
-        #print(mysql_connection.print_mysql_columns(t))
-    # Execute the SQL to create the table
-    #cursor.execute(create_table_sql)
-
-    # Define the SQL for listing all tables in the database
-    #show_tables_sql = "SHOW TABLES"
-
-    # Execute the SQL to list all tables
-    #cursor.execute(show_tables_sql)
-
-    # Fetch and print all tables
-    #tables = cursor.fetchall()
-    #for table in tables:
-    #    print(table)
-
-    # Close the connection
-    #conn.close()
 
 # Check if the script is run as the main module
 if __name__ == "__main__":
